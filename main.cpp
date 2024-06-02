d76d7b44e01bc9b79fd5043d6a6ed00a859f9019
Nikita Krenov <nikitabaram@gmail.com>
2024-06-02 22:39:19 +0300

    type height and width and drow this in *   This all

--- main.cpp
+++ main.cpp
@@ -4,7 +4,23 @@
 using namespace std;
 
 int main() {
+	int N, K;
+	cout << "Enter a height: ";
+	cin >> N;
+	cout << "Enter a width: ";
+	cin >> K;
 
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < K; j++) {
+			if (i == 0 || i == N - 1 || j == 0 || j == K - 1) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
 
 	system("pause");
 	return 0;
