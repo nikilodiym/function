dfcdfe7a3766c11d8f54838c8e0073e8b55b9f45
Nikita Krenov <nikitabaram@gmail.com>
2024-06-03 16:49:30 +0300

    factorial number and enter

--- main.cpp
+++ main.cpp
@@ -3,8 +3,24 @@
 
 using namespace std;
 
-int main() {
+int factorial(int n) {
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		return n * factorial(n - 1);
+	
+	}
+}
 
+int main() {
+	int num;
+
+	cout << "Enter the number for calculate the factorial: ";
+	cin >> num;
+
+	int result = factorial(num);
+	cout << "The factorial of " << num << " is " << result << endl;
 
 	system("pause");
 	return 0;
