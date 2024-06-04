090a9d905feac8af0679b80f70343d9af5354a58
Nikita Krenov <nikitabaram@gmail.com>
2024-06-04 22:10:10 +0300

    positive or negative num and Enter

--- main.cpp
+++ main.cpp
@@ -3,8 +3,25 @@
 
 using namespace std;
 
-int main() {
+bool pos_or_neg(int n) {
+	if (n > 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
+int main() {
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
+	if (pos_or_neg(num)) {
+		cout << "The number " << num <<" is positive." << endl;
+	}
+	else {
+		cout << "The number " << num << " is negative." << endl;
+	}
 
 	system("pause");
 	return 0;
