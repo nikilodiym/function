31473fed6f6f8e86414caeeb01335023043ee1b0
Nikita Krenov <nikitabaram@gmail.com>
2024-06-04 21:46:21 +0300

    cube number math

--- main.cpp
+++ main.cpp
@@ -3,8 +3,17 @@
 
 using namespace std;
 
-int main() {
+bool cube(int n) {
+	return n * n * n;
+}
 
+int main() {
+	int num;
+
+	cout << "Enter a number: ";
+	cin >> num;
+	int result = cube(num);
+	cout << "The cube of " << num << " is " << result << endl;
 
 	system("pause");
 	return 0;
