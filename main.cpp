3f6174498a17b62105cf38f169dd59ddecfaf771
Nikita Krenov <nikitabaram@gmail.com>
2024-06-04 21:39:38 +0300

    prime or not prime number

--- main.cpp
+++ main.cpp
@@ -3,8 +3,28 @@
 
 using namespace std;
 
-int main() {
+bool isFunction(int n) {
+	if (n <= 1) {
+		return false;
+	}
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0) {
+			return false;
+		}
+	}
+}
 
+int main() {
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
+
+	if (isFunction(num)) {
+		cout << num << " is a prime number." << endl;
+	}
+	else {
+		cout << num << " is not a prime number." << endl;
+	}
 
 	system("pause");
 	return 0;
