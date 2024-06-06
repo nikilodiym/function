5f94a58b74570c6d8de42981d512fa83a14ebbb7
Nikita Krenov <nikitabaram@gmail.com>
2024-06-06 18:41:07 +0300

    between two num and sum

--- main.cpp
+++ main.cpp
@@ -3,8 +3,23 @@
 
 using namespace std;
 
-int main() {
+bool sum_work(int a, int b) {
+	int sum = 0;
+	for (int i = min(a, b); i < max(a, b); i++) {
+		sum += i;
+	}
+}
 
+int main() {
+	int num1, num2;
+	cout << "Enter the first number: ";
+	cin >> num1;
+
+	cout << "Enter the second number: ";
+	cin >> num2;
+
+	int result = sum_work(num1, num2);
+	cout << "The sum of the numbers between " << num1 << " and " << num2 << " is " << result << endl;
 
 	system("pause");
 	return 0;
