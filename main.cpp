aa76612250e9aa2810f4d01e936415145ffe8d6d
Nikita Krenov <nikitabaram@gmail.com>
2024-06-06 18:36:58 +0300

    base and export file work

--- main.cpp
+++ main.cpp
@@ -3,7 +3,24 @@
 
 using namespace std;
 
+int power(int base, int exponent) {
+    int result = 1;
+    for (int i = 0; i < exponent; ++i) {
+        result *= base;
+    }
+    return result;
+}
+
 int main() {
+	int base, exponent;
+	cout << "Enter base: ";
+	cin >> base;
+
+	cout << "Enter exponent: ";
+	cin >> exponent;
+
+	int result = power(base, exponent);
+	cout << base << " raised to the power of " << exponent << " is " << result << endl;
 
 
 	system("pause");
