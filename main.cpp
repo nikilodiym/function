ded90f039372b65b6f5961de88e024736f945d2e
Nikita Krenov <nikitabaram@gmail.com>
2024-06-06 18:58:06 +0300

    card game

--- main.cpp
+++ main.cpp
@@ -3,8 +3,27 @@
 
 using namespace std;
 
-int main() {
+void print_card(int card) {
+	string num_card = to_string(card);
+	int width = num_card.length() + 4;
+	for (int i = 0; i < width; i++) {
+		cout << "*";
+	}
+	cout << endl;
+	cout << "* " << num_card << " *" << endl;
 
+	for (int i = 0; i < width; i++) {
+		cout << "*";
+	}
+	cout << endl;
+}
+
+int main() {
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
+
+	print_card(num);
 
 	system("pause");
 	return 0;
